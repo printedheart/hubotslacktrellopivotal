208801b164e172bd098ce136f8e462cb770724b2
printedheart <john@supportedreality.com>
2015-06-19 11:26:01 -0700

    Create trello.js

---
+++ trello.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Trello = require('node-trello');
+var trello = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function postToTrello(listId, command, text, cb) {
+  if (text == undefined || text == null || text == "") {
+    throw new Error('Format is ' + command + ' name | description(optional)');
+  }
+
+  var name_and_desc = text.split('|');
+
+	var card_data = {
+		'name' : name_and_desc.shift(),
+		'desc' : name_and_desc.shift()
+	};
+
+	trello.post('/1/lists/' + listId + '/cards', card_data, cb);
+
+});
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('SupportKit.io loves Slack and Trello!') });
+ 
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send('Error: ' + err.message);
+});
+
+app.listen(port, function () {
+  console.log('Started Slack-To-Trello ' + port);
+});
